696dbaa2b3488970e00e15c8f0b62d0927e29c49
Matteo Gaetzner <matteo.gaetzner@hhi.fraunhofer.de>
2023-08-30 23:19:18 +0200

    updated python3_host_prog for my mac

--- lua/local.lua
+++ lua/local.lua
@@ -2,7 +2,9 @@ local M = {}
 
 -- Path to your python3 binary that has access to pynvim and other
 -- libraries that your Neovim config depends on
-M.python3_host_prog = vim.fn.expand("/path/to/your/python3")
+-- M.python3_host_prog = vim.fn.expand("~/.conda/envs/neovim-python3/bin/python3")
+-- M.python3_host_prog = vim.fn.expand("~/.mambaforge/envs/neovim/bin/python3")
+M.python3_host_prog = vim.fn.expand("~/.local/share/micromamba/envs/neovim-python3/bin/python3")
 
 -- same for perl
 M.perl_host_prog = "/usr/bin/perl"
