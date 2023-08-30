738638857f4ed32665c82459946e886eb5ad075c
Matteo Gaetzner <matteo.gaetzner@hhi.fraunhofer.de>
2023-08-30 22:24:49 +0200

    Nvim tree

--- init.lua
+++ init.lua
@@ -11,6 +11,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- disable netrw (should be at the very start of init.lua)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
 -- Keymaps
 vim.g.mapleader = " "
 
@@ -27,7 +34,7 @@ local function file_exists(path)
 end
 
 -- Path to the custom Python 3 binary
-local custom_python3_path = vim.fn.expand("~/.local/share/micromamba/envs/neovim-python3/bin/python3")
+local custom_python3_path = vim.fn.expand("~/.conda/envs/neovim-python3/bin/pip3")
 
 -- Check if the custom Python 3 binary exists
 if file_exists(custom_python3_path) then
@@ -68,6 +75,7 @@ local plugins = {
 			"hrsh7th/cmp-cmdline",
 			"saadparwaiz1/cmp_luasnip",
 			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets"
 		},
 	},
 	{ "folke/neodev.nvim",    opts = {} },
@@ -95,13 +103,15 @@ local plugins = {
 		version = 'v1.x.x', -- version is optional, but recommended
 		dependencies = { 'neovim/nvim-lspconfig' },
 	},
-	{ 'ThePrimeagen/harpoon' },
+	{ 'nvim-tree/nvim-tree.lua' },
+	{ 'nvim-tree/nvim-web-devicons' },
 	{
 		'nvim-telescope/telescope.nvim',
 		tag = '0.1.2',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
 	{ 'numToStr/Comment.nvim', lazy = false },
+	{ 'gbprod/cutlass.nvim' },
 	{
 		"neanias/everforest-nvim",
 		version = false,
@@ -247,12 +257,6 @@ vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
 vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
 vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
 
--- Telescope fuzzy finder
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- Mason and lspconfig (Mason must come first!)
 require("mason").setup()
@@ -289,6 +293,14 @@ local efmls_config = {
 }
 
 local lspconfig = require('lspconfig')
+local lsp_defaults = lspconfig.util.default_config
+
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+	'force',
+	lsp_defaults.capabilities,
+	require('cmp_nvim_lsp').default_capabilities()
+)
+
 lspconfig.efm.setup(vim.tbl_extend('force', efmls_config, {}))
 lspconfig.pyright.setup {}
 lspconfig.rust_analyzer.setup {}
@@ -302,71 +314,124 @@ lspconfig.lua_ls.setup {
 	},
 }
 
+-- Snippets
+require('luasnip.loaders.from_vscode').lazy_load()
 
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+-- nvim-tree
+local function my_on_attach(bufnr)
+	local api = require("nvim-tree.api")
 
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-	callback = function(ev)
-		-- Enable completion triggered by <c-x><c-o>
-		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
 
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local opts = { buffer = ev.buf }
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-		vim.keymap.set('n', '<space>wl', function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, opts)
-		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', '<space>f', function()
-			vim.lsp.buf.format { async = true }
-		end, opts)
-	end,
-})
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
+	vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
+-- pass to setup along with your other options
+require("nvim-tree").setup {
+	on_attach = my_on_attach,
+}
+
 
 -- Cmp
-local cmp = require('cmp')
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
-cmp.setup({ ---@diagnostic disable-line
+require('luasnip.loaders.from_vscode').lazy_load()
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+local select_opts = { behavior = cmp.SelectBehavior.Select }
+
+---@diagnostic disable-next-line
+cmp.setup({
 	snippet = {
-		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			luasnip.lsp_expand(args.body)
+		end
+	},
+	sources = {
+		{ name = 'path' },
+		{ name = 'nvim_lsp', keyword_length = 1 },
+		{ name = 'buffer',   keyword_length = 3 },
+		{ name = 'luasnip',  keyword_length = 2 },
+	},
+	---@diagnostic disable-next-line
+	window = {
+		documentation = cmp.config.window.bordered()
+	},
+	---@diagnostic disable-next-line
+	formatting = {
+		fields = { 'menu', 'abbr', 'kind' },
+		format = function(entry, item)
+			local menu_icon = {
+				nvim_lsp = 'λ',
+				luasnip = '⋗',
+				buffer = 'Ω',
+				path = '🖫',
+			}
+
+			item.menu = menu_icon[entry.source.name]
+			return item
 		end,
 	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
+	---@diagnostic disable-next-line
+	mapping = {
+		['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+		['<Down>'] = cmp.mapping.select_next_item(select_opts),
+
+		['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+		['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
+
 		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "nvim_lua" },
-		{ name = "luasnip" },
-	}, {
-		{ name = "buffer" },
-		{ name = "path" },
-	})
+		['<C-l>'] = cmp.mapping.confirm({ select = true }),
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+		['<C-f>'] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(1) then
+				luasnip.jump(1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+
+		['<C-b>'] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+
+		['<Tab>'] = cmp.mapping(function(fallback)
+			local col = vim.fn.col('.') - 1
+
+			if cmp.visible() then
+				cmp.select_next_item(select_opts)
+			elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+				fallback()
+			else
+				cmp.complete()
+			end
+		end, { 'i', 's' }),
+
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item(select_opts)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+	},
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
@@ -381,3 +446,6 @@ cmp.setup.cmdline(':', { ---@diagnostic disable-line
 
 -- Format on save
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+-- Keymaps
+require("keymaps")
---
+++ lua/keymaps.lua
@@ -0,0 +1,98 @@
+local function map(mode, lhs, rhs, opts)
+	local keys = require("lazy.core.handler").handlers.keys
+	---@cast keys LazyKeysHandler
+	-- do not create the keymap if a lazy keys handler exists
+	if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+		opts = opts or {}
+		opts.silent = opts.silent ~= false
+		vim.keymap.set(mode, lhs, rhs, opts)
+	end
+end
+
+-- Moving lines
+map({ "n", "v" }, "<C-J>", ":move .+1<CR>==", { desc = "move line up" })
+map({ "n", "v" }, "<C-K>", ":move .-2<CR>==", { desc = "move line down" })
+map("i", "<C-J>", "<Esc>:move .+1<CR>==gi", { desc = "move line up" })
+map("i", "<C-K>", "<Esc>:move .-2<CR>==gi", { desc = "move line down" })
+
+-- Close tab
+map({ "n", "v" }, "<leader>d", "<cmd>bd<cr>", { desc = "Close Tab" })
+
+-- Go to tab by number
+map({ "n", "v" }, "<leader>1", "1gt", { desc = "go to tab 1" })
+map({ "n", "v" }, "<leader>2", "2gt", { desc = "go to tab 2" })
+map({ "n", "v" }, "<leader>3", "3gt", { desc = "go to tab 3" })
+map({ "n", "v" }, "<leader>4", "4gt", { desc = "go to tab 4" })
+map({ "n", "v" }, "<leader>5", "5gt", { desc = "go to tab 5" })
+map({ "n", "v" }, "<leader>6", "6gt", { desc = "go to tab 6" })
+map({ "n", "v" }, "<leader>7", "7gt", { desc = "go to tab 7" })
+map({ "n", "v" }, "<leader>8", "8gt", { desc = "go to tab 8" })
+map({ "n", "v" }, "<leader>9", "9gt", { desc = "go to tab 9" })
+map({ "n", "v" }, "<leader>0", ":tablast<CR>")
+
+-- Cut movements
+map("n", "m", "d", { desc = "normal mode cut" })
+map("x", "m", "d", { desc = "visual mode cut" })
+map("n", "mm", "dd", { desc = "normal mode cut line" })
+map("n", "M", "D", { desc = "visual mode cut" })
+
+-- Telescope fuzzy finder
+local builtin = require('telescope.builtin')
+map('n', '<leader>ff', builtin.find_files, {})
+map('n', '<leader>fg', builtin.live_grep, {})
+map('n', '<leader>fb', builtin.buffers, {})
+map('n', '<leader>fh', builtin.help_tags, {})
+
+-- Diagnostics
+map('n', '[d', vim.diagnostic.goto_prev)
+map('n', ']d', vim.diagnostic.goto_next)
+
+-- LSP stuff
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		map('n', 'gD', vim.lsp.buf.declaration, opts)
+		map('n', 'gd', vim.lsp.buf.definition, opts)
+		map('n', 'K', vim.lsp.buf.hover, opts)
+		map('n', 'gi', vim.lsp.buf.implementation, opts)
+		map('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+		map('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		map('n', '<space>wl', function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		map('n', '<space>D', vim.lsp.buf.type_definition, opts)
+		map('n', '<space>rn', vim.lsp.buf.rename, opts)
+		map({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+		map('n', 'gr', vim.lsp.buf.references, opts)
+	end,
+})
+
+-- NvimTree
+map('n', '<leader>e', ':NvimTreeFindFileToggle!<CR>', { noremap = true })
+local function on_attach(bufnr)
+	local api = require "nvim-tree.api"
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	map('n', 'l', api.node.open.edit, opts('Open'))
+	map('n', 't', api.node.open.tab, opts('Open as Tab'))
+end
+
+local tree = require("nvim-tree")
+
+local config = {
+	on_attach = on_attach,
+}
+
+tree.setup(config)
