9cf4761857c36b32420b93fc179e4c71adf7a75f
Matteo Gätzner <m.gaetzner@gmx.de>
2023-10-05 22:00:31 +0200

    update

--- init.lua
+++ init.lua
@@ -230,8 +230,8 @@ cmp.setup({
 	sources = {
 		{ name = 'path' },
 		{ name = 'nvim_lsp', keyword_length = 1 },
-		{ name = 'buffer',   keyword_length = 3 },
-		{ name = 'luasnip',  keyword_length = 2 },
+		{ name = 'buffer',   keyword_length = 1 },
+		{ name = 'luasnip',  keyword_length = 1 },
 	},
 	---@diagnostic disable-next-line
 	window = {
