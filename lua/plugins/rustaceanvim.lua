94807cbb1b23d98ac131c8f054076a7524bef22f
Matteo Gaetzner <m.gaetzner@gmx.de>
2025-03-16 13:31:35 +0100

    feat: all features activated by default for rust

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -2,4 +2,21 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
     lazy = false,   -- This plugin is already lazy
+    ft = { 'rust' },
+    config = function()
+        ---@type RustaceanOpts
+        vim.g.rustaceanvim = {
+            ---@type RustaceanLspClientOpts
+            server = {
+                default_settings = {
+                    -- rust-analyzer language server configuration
+                    ['rust-analyzer'] = {
+                        ['cargo'] = {
+                            ['features'] = 'all'
+                        }
+                    },
+                },
+            },
+        }
+    end
 }
